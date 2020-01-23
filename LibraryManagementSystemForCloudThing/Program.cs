5a3e57b38d95e6dc66cabc5443a697022cdbd503
hardikcloudthing <59838419+hardikcloudthing@users.noreply.github.com>
2020-01-23 10:27:49 +0530

    Add project files.

---
+++ LibraryManagementSystemForCloudThing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryManagementSystemForCloudThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
